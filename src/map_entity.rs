3843879c7d4a4abbe472554b266bb1a606a02381
Noxmore <noxmore@noxmore.dev>
2024-03-19 14:42:34 -0700

    Rename MapEntityPropertiesView::require to get

--- src/map_entity.rs
+++ src/map_entity.rs
@@ -31,8 +31,8 @@ pub struct MapEntityPropertiesView<'w> {
 
 impl<'w> MapEntityPropertiesView<'w> {
     /// Gets a property from this entity accounting for entity class hierarchy.
-    /// If the property is not defined, it attempts to get it's default.
-    pub fn require<T: TrenchBroomValue>(&self, key: &str) -> Result<T, MapEntityInsertionError> {
+    /// If the property is not defined, it attempts to get its default.
+    pub fn get<T: TrenchBroomValue>(&self, key: &str) -> Result<T, MapEntityInsertionError> {
         let Some(value_str) = self.entity.properties.get(key).or(self
             .tb_config
             .get_entity_property_default(self.entity.classname()?, key))
@@ -53,7 +53,7 @@ impl<'w> MapEntityPropertiesView<'w> {
     /// Extracts a transform from this entity using the properties `angles`, `origin`, and `scale`.
     /// If you are not using those for your transform, you probably shouldn't use this function.
     pub fn get_transform(&self) -> Transform {
-        let rotation = match self.require::<Vec3>("angles")/* .map(Vec3::z_up_to_y_up) */ {
+        let rotation = match self.get::<Vec3>("angles")/* .map(Vec3::z_up_to_y_up) */ {
             Ok(rot) => Quat::from_euler(
                 // Honestly, i don't know why this works, i got here through hours of trial and error
                 EulerRot::default(),
@@ -66,13 +66,13 @@ impl<'w> MapEntityPropertiesView<'w> {
 
         Transform {
             translation: self
-                .require::<Vec3>("origin")
+                .get::<Vec3>("origin")
                 .unwrap_or(Vec3::ZERO)
                 .trenchbroom_to_bevy_space(),
             rotation,
-            scale: match self.require::<f32>("scale") {
+            scale: match self.get::<f32>("scale") {
                 Ok(scale) => Vec3::splat(scale),
-                Err(_) => match self.require::<Vec3>("scale") {
+                Err(_) => match self.get::<Vec3>("scale") {
                     Ok(scale) => scale.trenchbroom_to_bevy_space(),
                     Err(_) => Vec3::ONE,
                 },
