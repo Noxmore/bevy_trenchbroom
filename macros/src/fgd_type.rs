0ac758a85a7b405f22069c4f84070ed64c8ac52a
noxmore <76889683+noxmore@users.noreply.github.com>
2025-01-21 21:42:54 -0800

    Add variant documentation in FgdType derive macro

--- macros/src/fgd_type.rs
+++ macros/src/fgd_type.rs
@@ -6,34 +6,48 @@ pub(super) fn fgd_type_derive(input: DeriveInput) -> TokenStream {
     let mut property_type_choices = Vec::new();
 
     let mut variant_idents = Vec::new();
-    let mut variant_literals = Vec::new();
+    let mut variants = Vec::new();
 
-    let mut valid_variants = Vec::new();
-    
     match data {
         Data::Enum(data) => {
-            for Variant { ident: variant_ident, fields, .. } in data.variants {
+            for Variant { ident: variant_ident, attrs, fields, .. } in data.variants {
                 if !fields.is_empty() {
                     panic!("Only unit vectors supported, variant {variant_ident} has fields");
                 }
 
-                valid_variants.push(variant_ident.to_string());
+                let mut doc = None;
 
-                let variant_literal = Literal::string(&variant_ident.to_string());
+                for attr in attrs {
+                    match attr.meta {
+                        Meta::NameValue(meta) => {
+                            if compare_path(&meta.path, "doc") {
+                                doc = Some(meta.value);
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+
+                variants.push(variant_ident.to_string());
+
                 let variant_literal_quoted = Literal::string(&format!("\"{variant_ident}\""));
 
+                let title = doc.unwrap_or_else(|| Expr::Lit(ExprLit {
+                    attrs: Vec::new(),
+                    lit: Lit::new(Literal::string(&variant_ident.to_string())),
+                }));
+
                 property_type_choices.push(quote! {
-                    (#variant_literal_quoted, #variant_literal),
+                    (#variant_literal_quoted, #title),
                 });
 
                 variant_idents.push(variant_ident);
-                variant_literals.push(variant_literal);
             }
         }
         _ => panic!("Currently only enums supported"),
     }
 
-    let valid_variants = valid_variants.join(", ");
+    let valid_variants = variants.join(", ");
 
     quote! {
         impl ::bevy_trenchbroom::fgd::FgdType for #ident {
@@ -43,14 +57,14 @@ pub(super) fn fgd_type_derive(input: DeriveInput) -> TokenStream {
 
             fn fgd_parse(input: &str) -> ::bevy_trenchbroom::anyhow::Result<Self> {
                 match input {
-                    #(#variant_literals => Ok(Self::#variant_idents),)*
+                    #(#variants => Ok(Self::#variant_idents),)*
                     input => Err(::bevy_trenchbroom::anyhow::anyhow!(concat!("{input} isn't a valid ", stringify!(#ident), "! Valid variants are ", #valid_variants))),
                 }
             }
 
             fn fgd_to_string(&self) -> String {
                 match self {
-                    #(Self::#variant_idents => #variant_literals.to_string(),)*
+                    #(Self::#variant_idents => #variants.to_string(),)*
                 }
             }
         }
