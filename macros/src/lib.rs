600abe9925a48605ff189a643bf4e03480e48500
noxmore <76889683+noxmore@users.noreply.github.com>
2025-02-05 17:56:29 -0800

    Add doc comments to derive macros

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -8,21 +8,57 @@ use syn::*;
 
 // TODO spawnflags support using something like bitflags?
 
+/// Point classes don't have any geometry built in -- simply a point in space.
+/// 
+/// If the `auto_register` feature is enabled, this will automatically register the type with Bevy and `bevy_trenchbroom`.
+/// 
+/// # Type attributes
+/// - `#[model(<path expression>)]` Displays the entity as the specified model in-editor.
+/// - `#[model({ "path": <path expr>, "skin": <skin expr>, "frame": <frame expr>, "scale": <scale expr> })]` Same as above attribute, but with greater control over how the model is shown. Note that any of these properties can be left out.
+/// - `#[color(<red> <green> <blue>)]` Changes the wireframe color of the entity. Each number has a range from 0 to 255.
+/// - `#[iconsprite(<path expression>)]` Displays the entity as the specified image, presented as a billboard (always facing the camera).
+/// - `#[size(<-x>, <-y>, <-z>, <+x>, <+y>, <+z>)]` The bounding box of the entity in-editor.
+/// - `#[classname(<case type>)]` Case type can be something like `PascalCase` or `snake_case`. Default if not specified is `snake_case`.
+/// - `#[classname(<string>)]` When outputted to fgd, use the specified string instead of a classname with case converted via the previous attribute.
+/// - `#[require(<type ...>)]` From `Component` macro, this also sets this entity's base classes in the `FGD.
+/// - `#[base(<type ...>)]` Overrides `require` attribute. Use if you want separate base classes and required components.
+///
+/// # Field attributes
+/// - `#[no_default]` Use on fields you want to output an error if not defined, rather than just being replaced by the field's default value.
 #[proc_macro_derive(PointClass, attributes(model, color, iconsprite, size, classname, base, no_default))]
 pub fn point_class_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 	quake_class::class_derive(parse_macro_input!(input as DeriveInput), quake_class::QuakeClassType::Point).into()
 }
 
+/// Solid classes contain brush geometry.
+/// 
+/// If the `auto_register` feature is enabled, this will automatically register the type with Bevy and `bevy_trenchbroom`.
+/// 
+/// # Type attributes
+/// - `#[geometry(<rust expression>)]` Required. An expression that produces a `GeometryProvider` to control how the geometry appears in the world.
+/// - `#[classname(<case type>)]` Case type can be something like `PascalCase` or `snake_case`. Default if not specified is `snake_case`.
+/// - `#[classname(<string>)]` When outputted to fgd, use the specified string instead of a classname with case converted via the previous attribute.
+/// - `#[require(<type ...>)]` From `Component` macro, this also sets this entity's base classes in the `FGD.
+/// - `#[base(<type ...>)]` Overrides `require` attribute. Use if you want separate base classes and required components.
+///
+/// # Field attributes
+/// - `#[no_default]` Use on fields you want to output an error if not defined, rather than just being replaced by the field's default value.
 #[proc_macro_derive(SolidClass, attributes(geometry, classname, base, no_default))]
 pub fn solid_class_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 	quake_class::class_derive(parse_macro_input!(input as DeriveInput), quake_class::QuakeClassType::Solid).into()
 }
 
+/// Base classes don't appear in-editor, rather they give properties and attributes to their sub-classes (components that require them).
+/// 
+/// If the `auto_register` feature is enabled, this will automatically register the type with Bevy and `bevy_trenchbroom`.
+/// 
+/// It has the same attributes as [`PointClass`].
 #[proc_macro_derive(BaseClass, attributes(model, color, iconsprite, size, classname, base, no_default))]
 pub fn base_class_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 	quake_class::class_derive(parse_macro_input!(input as DeriveInput), quake_class::QuakeClassType::Base).into()
 }
 
+/// Any field in quake class components must implement `FgdType`. Specifically, this macro implements it for unit enums, to create `choices` properties.
 #[proc_macro_derive(FgdType)]
 pub fn fgd_type_derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 	fgd_type::fgd_type_derive(parse_macro_input!(input as DeriveInput)).into()
