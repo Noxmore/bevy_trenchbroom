09df64898d31185af707dcdcd6b71eef20a86fa8
noxmore <76889683+noxmore@users.noreply.github.com>
2025-03-12 17:03:46 -0700

    Remove use of `vis` from `compile_map.sh`, we don't use any data from it anyway.

--- compile_map.sh
+++ compile_map.sh
@@ -3,14 +3,12 @@
 set -e
 
 # Meant to be used with ericw-tools in path
-qbsp -bsp2 -wrbrushesonly -nosubdivide -nosoftware -path assets/textures assets/maps/$1.map assets/maps/$1.bsp
-light -extra4 -novanilla -lightgrid -world_units_per_luxel 4 -path assets/textures assets/maps/$1.bsp
-vis assets/maps/$1.bsp
+qbsp -bsp2 -wrbrushesonly -nosubdivide -nosoftware -path "assets/textures" assets/maps/$1.map assets/maps/$1.bsp
+light -extra4 -novanilla -lightgrid -world_units_per_luxel 4 -path "assets/textures" assets/maps/$1.bsp
 
 # I like to remove the log files since they are just duplicates of what we get in the terminal
 rm assets/maps/$1.log
 rm assets/maps/$1-light.log
-rm assets/maps/$1-vis.log
 
 # Contains phong information, which currently we don't use.
 rm assets/maps/$1.texinfo.json
