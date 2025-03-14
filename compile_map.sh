3d8a4fbc941c774f20f26d7eca87d2280f8de169
noxmore <76889683+noxmore@users.noreply.github.com>
2025-03-14 14:45:47 -0700

    Note textures path hardcoded with ericw-tools and add -notex

--- compile_map.sh
+++ compile_map.sh
@@ -3,8 +3,8 @@
 set -e
 
 # Meant to be used with ericw-tools in path
-qbsp -bsp2 -wrbrushesonly -nosubdivide -nosoftware -path "assets/textures" assets/maps/$1.map assets/maps/$1.bsp
-light -extra4 -novanilla -lightgrid -world_units_per_luxel 4 -path "assets/textures" assets/maps/$1.bsp
+qbsp -bsp2 -wrbrushesonly -nosubdivide -nosoftware -path assets -notex assets/maps/$1.map assets/maps/$1.bsp
+light -extra4 -novanilla -lightgrid -world_units_per_luxel 4 -path assets assets/maps/$1.bsp
 
 # I like to remove the log files since they are just duplicates of what we get in the terminal
 rm assets/maps/$1.log
--- src/config.rs
+++ src/config.rs
@@ -52,6 +52,8 @@ pub struct TrenchBroomConfig {
 	package_format: AssetPackageFormat,
 
 	/// The root directory to look for textures in the [`assets_path`](Self::assets_path). (Default: "textures")
+	/// 
+	/// NOTE: If you are using ericw-tools, this is currently hardcoded to only work with "textures". ([see issue](https://github.com/ericwa/ericw-tools/issues/451))
 	#[default("textures".into())]
 	#[builder(into)]
 	pub material_root: PathBuf,
