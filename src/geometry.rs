9e0d0184c9d00ce91aea9169aa19910736d5a465
noxmore <76889683+noxmore@users.noreply.github.com>
2025-03-14 17:02:14 -0700

    Add `GeometryProvider::with` and `meshes_with`

--- src/geometry.rs
+++ src/geometry.rs
@@ -219,21 +219,30 @@ impl GeometryProvider {
 		self
 	}
 
+	/// Inserts a bundle onto the entity.
+	pub fn with(self, bundle: impl Bundle) -> Self {
+		self.push(move |view| {
+			view.world.entity_mut(view.entity).insert(bundle);
+		})
+	}
+	/// Inserts a bundle onto all mesh entities.
+	pub fn meshes_with(self, bundle: impl Bundle + Clone) -> Self {
+		self.push(move |view| {
+			for mesh_view in &view.meshes {
+				view.world.entity_mut(mesh_view.entity).insert(bundle.clone());
+			}
+		})
+	}
+
 	/// Inserts trimesh colliders on each mesh of this entity. This means that brushes will be hollow. Not recommended to use on physics objects.
 	#[cfg(any(feature = "rapier", feature = "avian"))]
 	pub fn trimesh_collider(self) -> Self {
-		self.push(|view| {
-			for mesh_view in &view.meshes {
-				view.world.entity_mut(mesh_view.entity).insert(physics::TrimeshCollision);
-			}
-		})
+		self.meshes_with(physics::TrimeshCollision)
 	}
 
 	/// Inserts a compound collider of every brush in this entity into said entity. Brushes will be fully solid.
 	#[cfg(any(feature = "rapier", feature = "avian"))]
 	pub fn convex_collider(self) -> Self {
-		self.push(|view| {
-			view.world.entity_mut(view.entity).insert(physics::ConvexCollision);
-		})
+		self.with(physics::ConvexCollision)
 	}
 }
