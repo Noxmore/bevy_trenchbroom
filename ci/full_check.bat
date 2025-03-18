8c32e329cdde503cff92cdfd748776a8a83a7c4e
noxmore <76889683+noxmore@users.noreply.github.com>
2025-03-18 14:05:20 -0700

    Move cargo fmt to the top of ci

--- ci/full_check.bat
+++ ci/full_check.bat
@@ -1,11 +1,11 @@
-cargo clippy --all-targets || exit /b 1
-
-cargo clippy --no-default-features --all-targets || exit /b 1
-
-cargo clippy --features rapier --all-targets || exit /b 1
-
-cargo clippy --features avian --all-targets || exit /b 1
-
 cargo fmt --all --check || exit /b 1
+
+cargo clippy --workspace || exit /b 1
+
+cargo clippy --no-default-features --workspace || exit /b 1
+
+cargo clippy --features rapier --workspace || exit /b 1
+
+cargo clippy --features avian --workspace || exit /b 1
 
 cargo test --features avian || exit /b 1
