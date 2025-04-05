f786940d3482369902c9a1434e6562f365638e2a
noxmore <76889683+noxmore@users.noreply.github.com>
2025-04-05 12:08:43 -0700

    Re-add task names in `full_check` scripts

--- ci/full_check.sh
+++ ci/full_check.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 set -e
+set -x
 
-echo "cargo fmt --check --all"
+# These commands correspond 1 to 1 with github actions.
+
+{ echo; echo; echo "Run cargo fmt"; } 2> /dev/null
 cargo fmt --check --all
 
-echo "cargo clippy --tests --examples"
+
+{ echo; echo; echo "Run cargo clippy for default features"; } 2> /dev/null
 cargo clippy --tests --examples
 
-echo "cargo clippy --tests --examples --no-default-features"
+{ echo; echo; echo "Run cargo clippy without default features"; } 2> /dev/null
 cargo clippy --tests --examples --no-default-features
 
-echo "cargo clippy --tests --examples --features avian"
+{ echo; echo; echo "Run cargo clippy for avian"; } 2> /dev/null
 cargo clippy --tests --examples --features avian
 
-echo "cargo clippy --tests --examples --features rapier"
+{ echo; echo; echo "Run cargo clippy for rapier"; } 2> /dev/null
 cargo clippy --tests --examples --features rapier
 
-echo "cargo test --workspace --doc --features bevy/x11 --features avian"
+
+{ echo; echo; echo "Run tests with avian"; } 2> /dev/null
 LD_LIBRARY_PATH="$(rustc --print target-libdir)" cargo test --locked --workspace --doc --features bevy/x11 --features avian
 
-echo "cargo doc --no-deps"
+
+{ echo; echo; echo "Run cargo doc with default features"; } 2> /dev/null
 cargo doc --no-deps
