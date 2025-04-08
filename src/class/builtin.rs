2f5c03655b48cdb6c14274b3b32fbc5f9ab0abf0
noxmore <76889683+noxmore@users.noreply.github.com>
2025-04-07 17:11:09 -0700

    Fix #[no_register] in `class/builtin.rs`

--- src/class/builtin.rs
+++ src/class/builtin.rs
@@ -437,7 +437,7 @@ impl QuakeClass for DirectionalLight {
 /// TODO: this is currently just a skeleton struct, first-class entity IO hasn't been added yet.
 #[derive(BaseClass, Component, Reflect, Debug, Clone, SmartDefault, Serialize, Deserialize)]
 #[reflect(Component, Default, Serialize, Deserialize)]
-#[no_register]
+#[class(no_register)]
 pub struct Target {
 	/// If [`Some`], when this entity's IO fires, it will activate all entities with its [`Targetable::targetname`] set to this, with whatever input that functionality that entity has set up.
 	pub target: Option<String>,
@@ -450,7 +450,7 @@ pub struct Target {
 /// TODO: this is currently just a skeleton struct, first-class entity IO hasn't been added yet.
 #[derive(BaseClass, Component, Reflect, Debug, Clone, SmartDefault, Serialize, Deserialize)]
 #[reflect(Component, Default, Serialize, Deserialize)]
-#[no_register]
+#[class(no_register)]
 pub struct Targetable {
 	/// The name for entities with [`Target`] components to point to.
 	pub targetname: Option<String>,
