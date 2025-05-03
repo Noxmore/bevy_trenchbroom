78941b412858b64d3ac22f52e76f7f74b49c5496
noxmore <76889683+noxmore@users.noreply.github.com>
2025-05-02 17:05:32 -0700

    Add `world_units_per_luxel` disclaimer in `compile_map.sh`

--- compile_map.sh
+++ compile_map.sh
@@ -5,6 +5,7 @@ set -e
 # Meant to be used with ericw-tools in path
 qbsp -bsp2 -wrbrushesonly -nosubdivide -nosoftware -path assets -notex assets/maps/$1.map assets/maps/$1.bsp
 light -extra4 -novanilla -lightgrid -world_units_per_luxel 4 -path assets assets/maps/$1.bsp
+# NOTE: -world_units_per_luxel 4 is quite high, use something more reasonable on larger maps.
 
 # I like to remove the log files since they are just duplicates of what we get in the terminal
 rm assets/maps/$1.log
