05274ae4f23b18f154a8637031d19bd4e7ab6b7f
Noxmore <76889683+noxmore@users.noreply.github.com>
2026-01-17 18:28:54 -0800

    Lock tangent generation behind `client` feature

--- src/brush.rs
+++ src/brush.rs
@@ -401,6 +401,8 @@ pub fn generate_mesh_from_brush_polygons(polygons: &[BrushSurfacePolygon], confi
 	}
 	mesh.insert_indices(Indices::U32(indices));
 
+	// Servers don't care about things like normal maps.
+	#[cfg(feature = "client")]
 	if !polygons.is_empty()
 		&& let Err(err) = mesh.generate_tangents()
 	{
