723f37287d1bb355dd07d7b6c2ccf0f2618a872b
Valentin Romanov <22451680+valrom@users.noreply.github.com>
2026-03-17 02:57:03 +0300

    Removed `DebugCamera` movement systems prior to Bevy's `FreeCamera`

--- example/example_commons/lib.rs
+++ example/example_commons/lib.rs
@@ -1,17 +1,6 @@
+#[cfg(feature = "client")]
+use bevy::camera_controller::free_camera;
 use bevy::prelude::*;
-#[cfg(feature = "client")]
-use bevy::{
-	input::mouse::AccumulatedMouseMotion,
-	window::CursorOptions,
-	window::{CursorGrabMode, PrimaryWindow},
-};
-
-// These are hardcoded because this is only for examples.
-
-#[cfg(feature = "client")]
-pub const SENSITIVITY: f32 = 0.002;
-#[cfg(feature = "client")]
-pub const BASE_MOVEMENT_SPEED: f32 = 4.;
 
 pub struct ExampleCommonsPlugin;
 impl Plugin for ExampleCommonsPlugin {
@@ -22,91 +11,15 @@ impl Plugin for ExampleCommonsPlugin {
 			.add_plugins((
 				bevy_inspector_egui::bevy_egui::EguiPlugin::default(),
 				bevy_inspector_egui::quick::WorldInspectorPlugin::default(),
+				free_camera::FreeCameraPlugin,
 			))
 			.add_systems(Update, (
-				Self::toggle_focus,
-				Self::move_debug_camera,
 				Self::draw_debug_transforms,
 			))
 		;
 	}
 }
 impl ExampleCommonsPlugin {
-	#[cfg(feature = "client")]
-	pub fn move_debug_camera(
-		mouse_motion: Res<AccumulatedMouseMotion>,
-		keyboard: Res<ButtonInput<KeyCode>>,
-		cursor_options: Single<&CursorOptions, With<PrimaryWindow>>,
-		mut camera_query: Query<&mut Transform, With<DebugCamera>>,
-		time: Res<Time>,
-	) {
-		if cursor_options.grab_mode == CursorGrabMode::None {
-			return;
-		}
-
-		for mut transform in &mut camera_query {
-			// Mouse movement
-			let (mut yaw, mut pitch, _) = transform.rotation.to_euler(EulerRot::YXZ);
-
-			pitch -= mouse_motion.delta.y * SENSITIVITY;
-			yaw -= mouse_motion.delta.x * SENSITIVITY;
-
-			pitch = pitch.clamp(-std::f32::consts::FRAC_PI_2, std::f32::consts::FRAC_PI_2);
-
-			transform.rotation = Quat::from_axis_angle(Vec3::Y, yaw) * Quat::from_axis_angle(Vec3::X, pitch);
-
-			// Keyboard movement
-			let mut movement = Vec3::ZERO;
-			if keyboard.pressed(KeyCode::KeyW) {
-				movement += *transform.forward()
-			}
-			if keyboard.pressed(KeyCode::KeyS) {
-				movement += *transform.back()
-			}
-			if keyboard.pressed(KeyCode::KeyA) {
-				movement += *transform.left()
-			}
-			if keyboard.pressed(KeyCode::KeyD) {
-				movement += *transform.right()
-			}
-			movement = movement.normalize_or_zero();
-			if keyboard.pressed(KeyCode::Space) {
-				movement.y += 1.
-			}
-			if keyboard.pressed(KeyCode::ControlLeft) {
-				movement.y -= 1.
-			}
-
-			movement *= BASE_MOVEMENT_SPEED;
-
-			if keyboard.pressed(KeyCode::AltLeft) {
-				movement *= 10.;
-			} else if keyboard.pressed(KeyCode::ShiftLeft) {
-				movement *= 3.;
-			}
-
-			transform.translation += movement * time.delta_secs();
-		}
-	}
-
-	#[cfg(feature = "client")]
-	pub fn toggle_focus(mut cursor_options: Single<&mut CursorOptions, With<PrimaryWindow>>, keyboard: Res<ButtonInput<KeyCode>>) {
-		if !keyboard.just_pressed(KeyCode::Escape) {
-			return;
-		}
-
-		match cursor_options.grab_mode {
-			CursorGrabMode::None => {
-				cursor_options.grab_mode = CursorGrabMode::Locked;
-				cursor_options.visible = false;
-			}
-			_ => {
-				cursor_options.grab_mode = CursorGrabMode::None;
-				cursor_options.visible = true;
-			}
-		}
-	}
-
 	#[cfg(feature = "client")]
 	pub fn draw_debug_transforms(
 		mut gizmos: Gizmos,
@@ -138,5 +51,5 @@ pub fn default_debug_camera_transform() -> Transform {
 
 #[cfg(feature = "client")]
 #[derive(Component)]
-#[require(Transform, Camera3d)]
+#[require(Transform, Camera3d, free_camera::FreeCamera)]
 pub struct DebugCamera;
